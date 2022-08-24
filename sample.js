6a0bb2d17504a64d2ac498038b6ad5962ee7cc40
Wesley Cheah <wesley.cheah@trentglobal.edu.sg>
2022-08-24 10:26:06 +0000

    Added sample products response from API call

---
+++ sample.js
@@ -0,0 +1,530 @@
+let products = [
+  {
+    "id": 1,
+    "model": "Metropolitan",
+    "weight": 26,
+    "diameter": 13,
+    "length": 126,
+    "description": "A fantastic value from Pilot, the Metropolitan is a favorite first fountain pen. This black metal bodied pen has a glossy black accent band, silver trim, and a steel nib. It comes with a black Pilot ink cartridge and a squeeze converter to use with bottled ink, or you can upgrade to a Con-40 converter if you prefer that style. It comes packaged in a gift box.",
+    "brand_id": 1,
+    "sale_status_id": 1,
+    "cap_type_id": 1,
+    "image_url": "http://res.cloudinary.com/drkogbwds/image/upload/v1661311811/o9za13zjcu8vekti4lf6.jpg",
+    "thumbnail_url": "https://res.cloudinary.com/drkogbwds/image/upload/c_limit,h_60,w_90/v1661311811/o9za13zjcu8vekti4lf6.jpg",
+    "brand": {
+      "id": 1,
+      "brand": "Pilot"
+    },
+    "fillingMechanisms": [
+      {
+        "id": 1,
+        "filling_mechanism": "Cartridge/Converter",
+        "_pivot_fountain_pen_id": 1,
+        "_pivot_filling_mechanism_id": 1
+      }
+    ],
+    "variants": [
+      {
+        "id": 1,
+        "image_url": "http://res.cloudinary.com/drkogbwds/image/upload/v1661312060/vu8nmnpuosa8g6reb35y.jpg",
+        "thumbnail_url": "https://res.cloudinary.com/drkogbwds/image/upload/c_limit,h_60,w_90/v1661312060/vu8nmnpuosa8g6reb35y.jpg",
+        "cost": 1899,
+        "stock": 100,
+        "nib_material_id": 1,
+        "nib_shape_id": 1,
+        "nib_size_id": 2,
+        "nib_flexibility_id": 2,
+        "color_id": 1,
+        "fountain_pen_id": 1,
+        "color": {
+          "id": 1,
+          "color": "Black"
+        },
+        "nibSize": {
+          "id": 2,
+          "nib_size": "Fine"
+        },
+        "nibShape": {
+          "id": 1,
+          "nib_shape": "Round"
+        },
+        "nibMaterial": {
+          "id": 1,
+          "nib_material": "Stainless Steel"
+        },
+        "nibFlexibility": {
+          "id": 2,
+          "nib_flexibility": "Hard"
+        }
+      },
+      {
+        "id": 2,
+        "image_url": "http://res.cloudinary.com/drkogbwds/image/upload/v1661312114/k9jos2pealtk0jvhxfpz.jpg",
+        "thumbnail_url": "https://res.cloudinary.com/drkogbwds/image/upload/c_limit,h_60,w_90/v1661312114/k9jos2pealtk0jvhxfpz.jpg",
+        "cost": 1899,
+        "stock": 100,
+        "nib_material_id": 1,
+        "nib_shape_id": 1,
+        "nib_size_id": 3,
+        "nib_flexibility_id": 2,
+        "color_id": 1,
+        "fountain_pen_id": 1,
+        "color": {
+          "id": 1,
+          "color": "Black"
+        },
+        "nibSize": {
+          "id": 3,
+          "nib_size": "Medium"
+        },
+        "nibShape": {
+          "id": 1,
+          "nib_shape": "Round"
+        },
+        "nibMaterial": {
+          "id": 1,
+          "nib_material": "Stainless Steel"
+        },
+        "nibFlexibility": {
+          "id": 2,
+          "nib_flexibility": "Hard"
+        }
+      },
+      {
+        "id": 3,
+        "image_url": "http://res.cloudinary.com/drkogbwds/image/upload/v1661312157/brvnmpd0m1yd3uwoi7hc.jpg",
+        "thumbnail_url": "https://res.cloudinary.com/drkogbwds/image/upload/c_limit,h_60,w_90/v1661312157/brvnmpd0m1yd3uwoi7hc.jpg",
+        "cost": 1899,
+        "stock": 100,
+        "nib_material_id": 1,
+        "nib_shape_id": 2,
+        "nib_size_id": 3,
+        "nib_flexibility_id": 2,
+        "color_id": 1,
+        "fountain_pen_id": 1,
+        "color": {
+          "id": 1,
+          "color": "Black"
+        },
+        "nibSize": {
+          "id": 3,
+          "nib_size": "Medium"
+        },
+        "nibShape": {
+          "id": 2,
+          "nib_shape": "Italic/Stub"
+        },
+        "nibMaterial": {
+          "id": 1,
+          "nib_material": "Stainless Steel"
+        },
+        "nibFlexibility": {
+          "id": 2,
+          "nib_flexibility": "Hard"
+        }
+      },
+      {
+        "id": 5,
+        "image_url": "http://res.cloudinary.com/drkogbwds/image/upload/v1661312674/zdjbpwyllxzxyoxgm2wt.jpg",
+        "thumbnail_url": "https://res.cloudinary.com/drkogbwds/image/upload/c_limit,h_60,w_90/v1661312674/zdjbpwyllxzxyoxgm2wt.jpg",
+        "cost": 1899,
+        "stock": 100,
+        "nib_material_id": 1,
+        "nib_shape_id": 1,
+        "nib_size_id": 2,
+        "nib_flexibility_id": 2,
+        "color_id": 4,
+        "fountain_pen_id": 1,
+        "color": {
+          "id": 4,
+          "color": "Blue"
+        },
+        "nibSize": {
+          "id": 2,
+          "nib_size": "Fine"
+        },
+        "nibShape": {
+          "id": 1,
+          "nib_shape": "Round"
+        },
+        "nibMaterial": {
+          "id": 1,
+          "nib_material": "Stainless Steel"
+        },
+        "nibFlexibility": {
+          "id": 2,
+          "nib_flexibility": "Hard"
+        }
+      },
+      {
+        "id": 6,
+        "image_url": "http://res.cloudinary.com/drkogbwds/image/upload/v1661312736/kqi8vlhrjkrwzsa3hdi5.jpg",
+        "thumbnail_url": "https://res.cloudinary.com/drkogbwds/image/upload/c_limit,h_60,w_90/v1661312736/kqi8vlhrjkrwzsa3hdi5.jpg",
+        "cost": 1899,
+        "stock": 100,
+        "nib_material_id": 1,
+        "nib_shape_id": 1,
+        "nib_size_id": 2,
+        "nib_flexibility_id": 2,
+        "color_id": 3,
+        "fountain_pen_id": 1,
+        "color": {
+          "id": 3,
+          "color": "Silver"
+        },
+        "nibSize": {
+          "id": 2,
+          "nib_size": "Fine"
+        },
+        "nibShape": {
+          "id": 1,
+          "nib_shape": "Round"
+        },
+        "nibMaterial": {
+          "id": 1,
+          "nib_material": "Stainless Steel"
+        },
+        "nibFlexibility": {
+          "id": 2,
+          "nib_flexibility": "Hard"
+        }
+      }
+    ],
+    "properties": [
+      {
+        "id": 1,
+        "property": "Postable",
+        "_pivot_fountain_pen_id": 1,
+        "_pivot_property_id": 1
+      },
+      {
+        "id": 6,
+        "property": "Metal",
+        "_pivot_fountain_pen_id": 1,
+        "_pivot_property_id": 6
+      }
+    ],
+    "saleStatus": {
+      "id": 1,
+      "sale_status": "Active"
+    },
+    "capType": {
+      "id": 1,
+      "cap_type": "Snap-cap"
+    }
+  },
+  {
+    "id": 2,
+    "model": "Preppy",
+    "weight": 13,
+    "diameter": 12,
+    "length": 122,
+    "description": "The Platinum Preppy fountain pen is one of the best values for the money! This is a fantastic starter fountain pen, or great for lending to a friend. This pen features a clear plastic body with blue accents, and a stainless steel nib. As a Japanese-made pen, the nib runs on the narrow side. The Preppy accepts proprietary Platinum ink cartridges (one blue cartridge is included) or a Platinum converter (sold separately) to fill with bottled ink.\r\n\r\nBy converting this pen to an eyedropper, you can fill the entire barrel with ink for maximum ink capacity! All you need to do is add an o-ring and a small amount of silicone grease to the threads (available separately).",
+    "brand_id": 3,
+    "sale_status_id": 1,
+    "cap_type_id": 1,
+    "image_url": "http://res.cloudinary.com/drkogbwds/image/upload/v1661312374/yxnumkxzztnrl4dd8ogt.jpg",
+    "thumbnail_url": "https://res.cloudinary.com/drkogbwds/image/upload/c_limit,h_60,w_90/v1661312374/yxnumkxzztnrl4dd8ogt.jpg",
+    "brand": {
+      "id": 3,
+      "brand": "Platinum"
+    },
+    "fillingMechanisms": [
+      {
+        "id": 1,
+        "filling_mechanism": "Cartridge/Converter",
+        "_pivot_fountain_pen_id": 2,
+        "_pivot_filling_mechanism_id": 1
+      },
+      {
+        "id": 2,
+        "filling_mechanism": "Eyedropper",
+        "_pivot_fountain_pen_id": 2,
+        "_pivot_filling_mechanism_id": 2
+      }
+    ],
+    "variants": [
+      {
+        "id": 4,
+        "image_url": "http://res.cloudinary.com/drkogbwds/image/upload/v1661312620/oumb8cxc7xsxs3xykogr.jpg",
+        "thumbnail_url": "https://res.cloudinary.com/drkogbwds/image/upload/c_limit,h_60,w_90/v1661312620/oumb8cxc7xsxs3xykogr.jpg",
+        "cost": 700,
+        "stock": 100,
+        "nib_material_id": 1,
+        "nib_shape_id": 1,
+        "nib_size_id": 1,
+        "nib_flexibility_id": 2,
+        "color_id": 4,
+        "fountain_pen_id": 2,
+        "color": {
+          "id": 4,
+          "color": "Blue"
+        },
+        "nibSize": {
+          "id": 1,
+          "nib_size": "Extra Fine"
+        },
+        "nibShape": {
+          "id": 1,
+          "nib_shape": "Round"
+        },
+        "nibMaterial": {
+          "id": 1,
+          "nib_material": "Stainless Steel"
+        },
+        "nibFlexibility": {
+          "id": 2,
+          "nib_flexibility": "Hard"
+        }
+      },
+      {
+        "id": 11,
+        "image_url": "http://res.cloudinary.com/drkogbwds/image/upload/v1661313115/hhpum3yuolhtra5ogmkl.jpg",
+        "thumbnail_url": "https://res.cloudinary.com/drkogbwds/image/upload/c_limit,h_60,w_90/v1661313115/hhpum3yuolhtra5ogmkl.jpg",
+        "cost": 600,
+        "stock": 100,
+        "nib_material_id": 1,
+        "nib_shape_id": 1,
+        "nib_size_id": 2,
+        "nib_flexibility_id": 2,
+        "color_id": 5,
+        "fountain_pen_id": 2,
+        "color": {
+          "id": 5,
+          "color": "Clear"
+        },
+        "nibSize": {
+          "id": 2,
+          "nib_size": "Fine"
+        },
+        "nibShape": {
+          "id": 1,
+          "nib_shape": "Round"
+        },
+        "nibMaterial": {
+          "id": 1,
+          "nib_material": "Stainless Steel"
+        },
+        "nibFlexibility": {
+          "id": 2,
+          "nib_flexibility": "Hard"
+        }
+      }
+    ],
+    "properties": [
+      {
+        "id": 1,
+        "property": "Postable",
+        "_pivot_fountain_pen_id": 2,
+        "_pivot_property_id": 1
+      },
+      {
+        "id": 3,
+        "property": "Demonstrator",
+        "_pivot_fountain_pen_id": 2,
+        "_pivot_property_id": 3
+      },
+      {
+        "id": 5,
+        "property": "Resin",
+        "_pivot_fountain_pen_id": 2,
+        "_pivot_property_id": 5
+      }
+    ],
+    "saleStatus": {
+      "id": 1,
+      "sale_status": "Active"
+    },
+    "capType": {
+      "id": 1,
+      "cap_type": "Snap-cap"
+    }
+  },
+  {
+    "id": 3,
+    "model": "Vista",
+    "weight": 17,
+    "diameter": 12,
+    "length": 128,
+    "description": "This clear demonstrator LAMY vista fountain pen, essentially the clear version of the LAMY safari, has silver trim and an interchangeable silver steel nib. It features a triangular grip for proper hand position when writing. It comes with a blue LAMY ink cartridge to get you started writing right away.",
+    "brand_id": 2,
+    "sale_status_id": 1,
+    "cap_type_id": 1,
+    "image_url": "http://res.cloudinary.com/drkogbwds/image/upload/v1661312777/dsrj0bporzji91ig6jkd.jpg",
+    "thumbnail_url": "https://res.cloudinary.com/drkogbwds/image/upload/c_limit,h_60,w_90/v1661312777/dsrj0bporzji91ig6jkd.jpg",
+    "brand": {
+      "id": 2,
+      "brand": "LAMY"
+    },
+    "fillingMechanisms": [
+      {
+        "id": 1,
+        "filling_mechanism": "Cartridge/Converter",
+        "_pivot_fountain_pen_id": 3,
+        "_pivot_filling_mechanism_id": 1
+      }
+    ],
+    "variants": [
+      {
+        "id": 7,
+        "image_url": "http://res.cloudinary.com/drkogbwds/image/upload/v1661312932/nsz6z8zname2uw1eb0wc.jpg",
+        "thumbnail_url": "https://res.cloudinary.com/drkogbwds/image/upload/c_limit,h_60,w_90/v1661312932/nsz6z8zname2uw1eb0wc.jpg",
+        "cost": 2960,
+        "stock": 100,
+        "nib_material_id": 1,
+        "nib_shape_id": 1,
+        "nib_size_id": 1,
+        "nib_flexibility_id": 2,
+        "color_id": 5,
+        "fountain_pen_id": 3,
+        "color": {
+          "id": 5,
+          "color": "Clear"
+        },
+        "nibSize": {
+          "id": 1,
+          "nib_size": "Extra Fine"
+        },
+        "nibShape": {
+          "id": 1,
+          "nib_shape": "Round"
+        },
+        "nibMaterial": {
+          "id": 1,
+          "nib_material": "Stainless Steel"
+        },
+        "nibFlexibility": {
+          "id": 2,
+          "nib_flexibility": "Hard"
+        }
+      },
+      {
+        "id": 8,
+        "image_url": "http://res.cloudinary.com/drkogbwds/image/upload/v1661312975/iyr0g6929dwerz6cc7fp.jpg",
+        "thumbnail_url": "https://res.cloudinary.com/drkogbwds/image/upload/c_limit,h_60,w_90/v1661312975/iyr0g6929dwerz6cc7fp.jpg",
+        "cost": 2960,
+        "stock": 100,
+        "nib_material_id": 1,
+        "nib_shape_id": 1,
+        "nib_size_id": 2,
+        "nib_flexibility_id": 2,
+        "color_id": 5,
+        "fountain_pen_id": 3,
+        "color": {
+          "id": 5,
+          "color": "Clear"
+        },
+        "nibSize": {
+          "id": 2,
+          "nib_size": "Fine"
+        },
+        "nibShape": {
+          "id": 1,
+          "nib_shape": "Round"
+        },
+        "nibMaterial": {
+          "id": 1,
+          "nib_material": "Stainless Steel"
+        },
+        "nibFlexibility": {
+          "id": 2,
+          "nib_flexibility": "Hard"
+        }
+      },
+      {
+        "id": 9,
+        "image_url": "http://res.cloudinary.com/drkogbwds/image/upload/v1661313007/l0juypookonbu4jtcwkr.jpg",
+        "thumbnail_url": "https://res.cloudinary.com/drkogbwds/image/upload/c_limit,h_60,w_90/v1661313007/l0juypookonbu4jtcwkr.jpg",
+        "cost": 2960,
+        "stock": 100,
+        "nib_material_id": 1,
+        "nib_shape_id": 1,
+        "nib_size_id": 3,
+        "nib_flexibility_id": 2,
+        "color_id": 5,
+        "fountain_pen_id": 3,
+        "color": {
+          "id": 5,
+          "color": "Clear"
+        },
+        "nibSize": {
+          "id": 3,
+          "nib_size": "Medium"
+        },
+        "nibShape": {
+          "id": 1,
+          "nib_shape": "Round"
+        },
+        "nibMaterial": {
+          "id": 1,
+          "nib_material": "Stainless Steel"
+        },
+        "nibFlexibility": {
+          "id": 2,
+          "nib_flexibility": "Hard"
+        }
+      },
+      {
+        "id": 10,
+        "image_url": "http://res.cloudinary.com/drkogbwds/image/upload/v1661313043/jjzpd7i5zxxt9rhsbcxs.jpg",
+        "thumbnail_url": "https://res.cloudinary.com/drkogbwds/image/upload/c_limit,h_60,w_90/v1661313043/jjzpd7i5zxxt9rhsbcxs.jpg",
+        "cost": 2960,
+        "stock": 100,
+        "nib_material_id": 1,
+        "nib_shape_id": 1,
+        "nib_size_id": 4,
+        "nib_flexibility_id": 2,
+        "color_id": 5,
+        "fountain_pen_id": 3,
+        "color": {
+          "id": 5,
+          "color": "Clear"
+        },
+        "nibSize": {
+          "id": 4,
+          "nib_size": "Broad"
+        },
+        "nibShape": {
+          "id": 1,
+          "nib_shape": "Round"
+        },
+        "nibMaterial": {
+          "id": 1,
+          "nib_material": "Stainless Steel"
+        },
+        "nibFlexibility": {
+          "id": 2,
+          "nib_flexibility": "Hard"
+        }
+      }
+    ],
+    "properties": [
+      {
+        "id": 1,
+        "property": "Postable",
+        "_pivot_fountain_pen_id": 3,
+        "_pivot_property_id": 1
+      },
+      {
+        "id": 3,
+        "property": "Demonstrator",
+        "_pivot_fountain_pen_id": 3,
+        "_pivot_property_id": 3
+      },
+      {
+        "id": 5,
+        "property": "Resin",
+        "_pivot_fountain_pen_id": 3,
+        "_pivot_property_id": 5
+      }
+    ],
+    "saleStatus": {
+      "id": 1,
+      "sale_status": "Active"
+    },
+    "capType": {
+      "id": 1,
+      "cap_type": "Snap-cap"
+    }
+  }
+];
