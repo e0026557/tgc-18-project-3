d00085c1fe7c37d04caf77ae9a7e890ad2d3e7dc
Wesley Cheah <wesley.cheah@trentglobal.edu.sg>
2022-08-26 02:05:20 +0000

    Fixed typo for class attribute in Spinner.js

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -2,11 +2,11 @@ import React from "react";
 
 export default function Spinner() {
   return (
-    <div class="sk-folding-cube">
-      <div class="sk-cube1 sk-cube"></div>
-      <div class="sk-cube2 sk-cube"></div>
-      <div class="sk-cube4 sk-cube"></div>
-      <div class="sk-cube3 sk-cube"></div>
+    <div className="sk-folding-cube">
+      <div className="sk-cube1 sk-cube"></div>
+      <div className="sk-cube2 sk-cube"></div>
+      <div className="sk-cube4 sk-cube"></div>
+      <div className="sk-cube3 sk-cube"></div>
     </div>
   )
 }
