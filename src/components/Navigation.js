6bff8ea12b01edd769f8747c791a692b1be8985d
Wesley Cheah <wesley.cheah@trentglobal.edu.sg>
2022-08-27 10:51:11 +0000

    Removed redundant dependencies

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,10 @@
 // *** DEPENDENCIES ***
-import { useContext, useEffect, useState } from 'react';
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import Button from 'react-bootstrap/Button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 
