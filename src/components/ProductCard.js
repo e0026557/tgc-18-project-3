ef385d1d02d81910acf318a127d8a7f059579321
Wesley Cheah <wesley.cheah@trentglobal.edu.sg>
2022-08-28 08:42:25 +0000

    Added display of min cost in ProductCard

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -8,11 +8,23 @@ export default function ProductCard(props) {
   const imageUrl = props.product.image_url;
   const productName = `${props.product.brand.brand} ${props.product.model}`;
 
+  const variants = props.product.variants;
+  // Get min cost of product variant
+  let minCost = Infinity;
+  for (let variant of variants) {
+    if (variant.cost < minCost) {
+      minCost = variant.cost;
+    }
+  }
+  // Format min cost
+  minCost = Math.floor(minCost / 100);
+
   return (
     <Card className="card-product">
       <Card.Img variant="top" src={imageUrl} />
       <Card.Body className="d-flex flex-column justify-content-between">
         <Card.Title>{productName}</Card.Title>
+        <Card.Text>From ${minCost}</Card.Text>
         <Button className="mt-3" variant="primary" as={Link} to={`/products/${props.product.id}/view`}>View</Button>
       </Card.Body>
     </Card>
