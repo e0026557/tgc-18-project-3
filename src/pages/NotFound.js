eeb39df440e797f341c6ba8b78a6762d8abf276b
Wesley Cheah <wesley.cheah@trentglobal.edu.sg>
2022-08-30 10:44:14 +0000

    Added NotFound page which redirects back to home page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,9 +1,22 @@
-import React from 'react';
+// *** DEPENDENCIES ***
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function NotFound() {
-  return (
-    <React.Fragment>
-      Oops! Page does not exist!
-    </React.Fragment>
-  )
+	const navigateTo = useNavigate();
+
+	useEffect(() => {
+		setTimeout(() => {
+			navigateTo("/");
+		}, 3000);
+	}, []);
+
+	return (
+		<React.Fragment>
+			<div className='container d-flex flex-column justify-content-center align-items-center adjust-margin-top pt-5'>
+				<h3>Oops Page Does Not Exist</h3>
+				<p>Redirecting ...</p>
+			</div>
+		</React.Fragment>
+	);
 }
