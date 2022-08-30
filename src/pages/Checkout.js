95dd9c39520a2d02ce8fca7c0346bd140a739266
Wesley Cheah <wesley.cheah@trentglobal.edu.sg>
2022-08-30 10:41:33 +0000

    Added redirect back to home if there is an error during checkout

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -1,5 +1,6 @@
 // *** DEPENDENCIES ***
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
+import { useNavigate } from 'react-router-dom';
 import { loadStripe } from '@stripe/stripe-js';
 
 // *** CONTEXTS ***
@@ -8,19 +9,35 @@ import UserContext from '../contexts/UserContext';
 export default function Checkout() {
   // Consume user context
   const userContext = useContext(UserContext);
-    (async () => {
-      // Get stripe session id as object and publishable key
-      let sessionIdObj = {};
-      let publishableKey = null;
 
-      const stripeData = await userContext.checkout();
+  const navigateTo = useNavigate();
 
-      if (stripeData) {
-        sessionIdObj = {sessionId: stripeData.sessionId};
-        publishableKey = stripeData.publishableKey;
-      }
+  (async () => {
+    // Get stripe session id as object and publishable key
+    let sessionIdObj = {};
+    let publishableKey = null;
 
-      const stripe = await loadStripe(publishableKey);
-      stripe.redirectToCheckout(sessionIdObj);
-    })();
+    const stripeData = await userContext.checkout();
+    if (!stripeData) {
+      navigateTo('/')
+      return;
+    }
+
+    if (stripeData) {
+      sessionIdObj = { sessionId: stripeData.sessionId };
+      publishableKey = stripeData.publishableKey;
+    }
+
+    const stripe = await loadStripe(publishableKey);
+    stripe.redirectToCheckout(sessionIdObj);
+  })();
+
+  return (
+    <React.Fragment>
+      <div className='container d-flex flex-column justify-content-center align-items-center adjust-margin-top pt-5'>
+        <h3>Checking out cart</h3>
+        <p>Redirecting ...</p>
+      </div>
+    </React.Fragment>
+  )
 }
