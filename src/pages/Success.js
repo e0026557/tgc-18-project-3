9ad7e84d4c057e3b942bfdde16ed3f6c8d7b77ae
Wesley Cheah <wesley.cheah@trentglobal.edu.sg>
2022-08-30 10:41:00 +0000

    Added redirect to checkout success and fail pages

--- src/pages/Success.js
+++ src/pages/Success.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function Success() {
+
+	const navigateTo = useNavigate();
+
+	useEffect(() => {
+		setTimeout(() => {
+			navigateTo("/");
+		}, 3000);
+	}, []);
+
 	return (
 		<React.Fragment>
-			<div className='adjust-margin-top'></div>
-			Checkout success. Redirecting ...
+			<div className='container d-flex flex-column justify-content-center align-items-center adjust-margin-top pt-5'>
+				<h3>Checkout success</h3>
+				<p>Redirecting ...</p>
+			</div>
 		</React.Fragment>
 	);
 }
